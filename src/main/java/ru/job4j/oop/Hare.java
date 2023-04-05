20b05d5a6faeffd7a2ad5f9444292af48df6dfb5
Serge-In <aa11@mail.ru>
2023-04-05 21:08:02 +0300

    1.6. Взаимодействие объектов [#166087] refactor

--- src/main/java/ru/job4j/oop/Hare.java
+++ src/main/java/ru/job4j/oop/Hare.java
@@ -2,6 +2,6 @@ package ru.job4j.oop;
 
 public class Hare {
     public void tryEat(Ball ball) {
-        ball.tryRun(false);
+        System.out.println("Hare says: Hey, Ball! I'll eat You! " + ball.tryRun(false));
     }
 }
--- src/main/java/ru/job4j/oop/Wolf.java
+++ src/main/java/ru/job4j/oop/Wolf.java
@@ -2,6 +2,6 @@ package ru.job4j.oop;
 
 public class Wolf {
     public void tryEat(Ball ball) {
-        ball.tryRun(false);
+        System.out.println("Wolf says: Hey, Ball! I'll eat You! " + ball.tryRun(false));
     }
 }
