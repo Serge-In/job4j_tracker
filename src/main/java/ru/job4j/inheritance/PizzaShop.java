7430586217dee28824282554589239b95f5bb441
Serge-In <aa11@mail.ru>
2023-04-12 03:05:13 +0300

    6. Вызов переопределенного метода родителя - super.method ... [#504871]

---
+++ src/main/java/ru/job4j/inheritance/PizzaShop.java
@@ -0,0 +1,16 @@
+package ru.job4j.inheritance;
+
+/**
+ * 6. Вызов переопределенного метода родителя - super.method ... [#504871]
+ * 2023-04-12
+ */
+public class PizzaShop {
+    public static void main(String[] args) {
+        Pizza pizza = new Pizza();
+        PizzaExtraCheese pizzaExtraCheese = new PizzaExtraCheese();
+        PizzaExtraCheeseExtraTomato pizzaExtraCheeseExtraTomato = new PizzaExtraCheeseExtraTomato();
+        System.out.println(pizza.name());
+        System.out.println(pizzaExtraCheese.name());
+        System.out.println(pizzaExtraCheeseExtraTomato.name());
+    }
+}
