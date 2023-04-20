3c1e0b7cacb915bc887bfe1f94349b26f13a72d1
Serge-In <aa11@mail.ru>
2023-04-20 21:44:03 +0300

    13. Работа со строками. StringBuilder, StringJoiner, StringBuffer. [#504915]

---
+++ src/main/java/ru/job4j/concat/ConcatTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.concat;
+
+/**
+ * 2023-04-20
+ * 13. Работа со строками. StringBuilder, StringJoiner, StringBuffer. [#504915]
+ * //        String
+ * //        StringBuilder
+ * //        StringJoiner
+ * //        String.concat()
+ * //        String.join()
+ * //        StringBuffer
+ */
+public class ConcatTest {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        // было Миллисекунд: 166
+//        String str = "Job4j";
+//        for (int index = 0; index < 9999; index++) {
+//            str = str + index;
+//        }
+
+        // стало Миллисекунд: 2
+//        StringBuilder str = new StringBuilder("Job4j");
+//        for (int index = 0; index < 9999; index++) {
+//            str.append(index);
+//        }
+
+        // стало Миллисекунд: 9
+//        String str = "Job4j";
+//        for (int index = 0; index < 9999; index++) {
+//            str.concat(String.valueOf(index));
+//        }
+
+
+        System.out.println("Миллисекунд: " + (System.currentTimeMillis() - startTime));
+    }
+}
