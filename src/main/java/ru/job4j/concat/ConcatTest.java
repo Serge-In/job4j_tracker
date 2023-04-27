e32fd93c8c005fa2ba492b838d1f0fb66da14b01
Serge-In <geotod@mail.ru>
2023-04-27 23:05:34 +0300

    ConcatTest ref

--- src/main/java/ru/job4j/concat/ConcatTest.java
+++ src/main/java/ru/job4j/concat/ConcatTest.java
@@ -3,30 +3,34 @@ package ru.job4j.concat;
 /**
  * 2023-04-20
  * 13. Работа со строками. StringBuilder, StringJoiner, StringBuffer. [#504915]
- *         String
- *         StringBuilder
- *         StringJoiner
- *         String.concat()
- *         String.join()
- *         StringBuffer
+ * String
+ * StringBuilder
+ * StringJoiner
+ * String.concat()
+ * String.join()
+ * StringBuffer
  */
 public class ConcatTest {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
 /**
- *         было Миллисекунд: 166
- *         String str = "Job4j";
- *         for (int index = 0; index < 9999; index++) {
- *             str = str + index;
- *         }
- *
- *         стало Миллисекунд: 2
- *         StringBuilder str = new StringBuilder("Job4j");
- *         for (int index = 0; index < 9999; index++) {
- *             str.append(index);
- *         }
- *
- *          стало Миллисекунд: 9
+ * //было Миллисекунд: Asus 166, Dell 178, 162, 140
+ * String str = "Job4j";
+ * for (int index = 0; index < 9999; index++) {
+ * str = str + index;
+ * }
+ */
+
+/**
+ * //стало Миллисекунд: Asus 2, Dell 3, 3, 3
+ * StringBuilder str = new StringBuilder("Job4j");
+ * for (int index = 0; index < 9999; index++) {
+ * str.append(index);
+ * }
+ */
+
+/**
+ * //стало Миллисекунд: Asus 9, Dell 12, 11, 11
  *         String str = "Job4j";
  *         for (int index = 0; index < 9999; index++) {
  *             str.concat(String.valueOf(index));
