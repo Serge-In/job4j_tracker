0583cfc5648bcffe44a51e9e2c2d64f70951ec57
Serge-In <aa11@mail.ru>
2023-06-18 01:35:33 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365  [#219365]]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,24 @@
+package ru.job4j.ex;
+
+/**
+ * 2023-06-18
+ * 0.2. Кидаем исключение - throw new RuntimeException [#219365  [#219365]]
+ * Добавьте проверку входного параметра в метод calc. Если n < 0, то нужно прервать
+ * программу с исключением IllegalArgumentException("N could not be less than 0").
+ */
+public class Fact {
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("N could not be less than 0");
+        }
+        int rsl = 1;
+        for (int index = 2; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Fact().calc(-1));
+    }
+}
