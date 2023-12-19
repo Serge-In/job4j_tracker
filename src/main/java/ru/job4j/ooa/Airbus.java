237c05800a2926242bac223c100429f2774491b9
Serge-In <aa11@mail.ru>
2023-12-19 10:36:33 +0300

    3. final [#334073] err

---
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -0,0 +1,38 @@
+package ru.job4j.ooa;
+
+public class Airbus extends Aircraft {
+    private static final int COUNT_ENGINE = 4;
+
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    //printModel() in ru.job4j.ooa.Airbus cannot override printModel() in ru.job4j.ooa.Aircraft
+    //  overridden method is final
+    public void printModel() {
+        System.out.println("Модель самолета: " + name);
+    }
+
+    public void printCountEngine() {
+        COUNT_ENGINE = 4;
+        System.out.println("Количество двигателей равно: " + COUNT_ENGINE);
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
---
+++ src/main/java/ru/job4j/ooa/Airport.java
@@ -0,0 +1,27 @@
+package ru.job4j.ooa;
+/**
+ * 2023-12-19
+ * 3. final [#334073]
+ *  1. Ниже представлен код, он содержит ряд ошибок компиляции. Вам необходимо его исправить.
+ *  Нужно выполнить следующее:
+ * - сделать так, чтобы от класса Airbus нельзя было наследоваться.
+ * - реализовать метод printCountEngine().
+ * если модель самолета A380 - количество двигателей должно равняться 4,
+ * для всех остальных моделей - используем в качестве значения константу COUNT_ENGINE.
+ * Чтобы не допускать дублирования кода следует использовать дополнительную переменную и тернарный оператор.
+ * Код метода должен уместиться на двух строках.
+ */
+public class Airport {
+    public static void main(String[] args) {
+        final Airbus airbus = new Airbus("A320");
+        System.out.println(airbus);
+        airbus.printModel();
+        airbus.printCountEngine();
+
+        airbus = new Airbus("A380");
+        System.out.println(airbus);
+
+        airbus.setName("A380");
+        System.out.println(airbus);
+    }
+}
