56eb90ed5d8d2d526611f25953eee29d9c3d5d59
Serge-In <aa11@mail.ru>
2023-12-22 18:40:56 +0300

    4. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/main/java/ru/job4j/search/Person.java
@@ -0,0 +1,36 @@
+package ru.job4j.search;
+
+/**
+ * 2023-12-22
+ * 4. Телефонный справочник на базе ArrayList [#41598]
+ */
+
+public class Person {
+    private String name;
+    private String surname;
+    private String phone;
+    private String address;
+
+    public Person(String name, String surname, String phone, String address) {
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,35 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+/**
+ * 2023-12-22
+ * 4. Телефонный справочник на базе ArrayList [#41598]
+ * Вернуть список всех пользователей, который содержат key в любых полях.
+ * param key Ключ поиска.
+ * return Список пользователей, которые прошли проверку.
+ */
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (person.getName().contains(key)) {
+                result.add(person);
+            } else if (person.getSurname().contains(key)) {
+                result.add(person);
+            } else if (person.getPhone().contains(key)) {
+                result.add(person);
+            } else if (person.getAddress().contains(key)) {
+                result.add(person);
+            }
+        }
+        return result;
+    }
+}
