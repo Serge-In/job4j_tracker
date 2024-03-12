1f6f8b780796b39a34eb99b7cdca2bbc92cb4459
Serge-In <aa11@mail.ru>
2024-03-12 14:21:59 +0300

    2.3. Встроенные функциональные интерфейсы. [#249209]

---
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -0,0 +1,43 @@
+package ru.job4j.function;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * 2024-03-11
+ * 2.3. Встроенные функциональные интерфейсы. [#249209]
+ * Ваша задача - реализовать универсальный метод filter(), для проверки в if() в нем вы будете использовать функциональный интерфейс Predicate.
+ * После реализации - методы filterSize() и filterName() нужно будет удалить.
+ */
+public class SearchFolder {
+    public static List<Folder> filterSize(List<Folder> list) {
+        List<Folder> result = new ArrayList<>();
+        for (Folder folder : list) {
+            if (folder.getSize() > 100) {
+                result.add(folder);
+            }
+        }
+        return result;
+    }
+
+    public static List<Folder> filterName(List<Folder> list) {
+        List<Folder> result = new ArrayList<>();
+        for (Folder folder : list) {
+            if (folder.getName().contains("bug")) {
+                result.add(folder);
+            }
+        }
+        return result;
+    }
+
+    public static List<Folder> filter(List<Folder> list, Predicate<Folder> predicate) {
+        List<Folder> result = new ArrayList<>();
+        for (Folder folder : list) {
+            if (predicate.test(folder)) {
+                result.add(folder);
+            }
+        }
+        return result;
+    }
+}
