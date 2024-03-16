33775e434fcb71556a1d7ad0d8764775cfbdd188
Serge-In <aa11@mail.ru>
2024-03-16 14:30:00 +0300

    7. Функции высшего порядка [#24255 #486009] fix

--- src/main/java/ru/job4j/search/PhoneDictionaryFunc.java
+++ src/main/java/ru/job4j/search/PhoneDictionaryFunc.java
@@ -1,7 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
-import java.util.function.BiPredicate;
+import java.util.function.Predicate;
 
 /**
  * 2024-03-13
@@ -11,25 +11,20 @@ import java.util.function.BiPredicate;
  * Допишите метод find с использованием функции высшего порядка
  */
 public class PhoneDictionaryFunc {
-    private ArrayList<Person> persons = new ArrayList<Person>();
+    private ArrayList<Person> persons = new ArrayList<>();
 
     public void add(Person person) {
         this.persons.add(person);
     }
 
     public ArrayList<Person> find(String key) {
-        BiPredicate<String, Person> name = (x, y) -> x.equals(y.getName());
-        BiPredicate<String, Person> surname = (x, y) -> x.equals(y.getSurname());
-        BiPredicate<String, Person> phone = (x, y) -> x.equals(y.getPhone());
-        BiPredicate<String, Person> address = (x, y) -> x.equals(y.getAddress());
-        BiPredicate<String, Person> combine = name.or(surname).or(phone).or(address);
+        Predicate<Person> name = (person) -> person.getName().contains(key);
+        Predicate<Person> surname = (person) -> person.getSurname().contains(key);
+        Predicate<Person> phone = (person) -> person.getPhone().contains(key);
+        Predicate<Person> address = (person) -> person.getAddress().contains(key);
 
-        ArrayList<Person> result = new ArrayList<>();
-        for (Person person : persons) {
-            if (combine.test(key, person)) {
-                result.add(person);
-            }
-        }
-        return result;
+        Predicate<Person> combine = name.or(surname).or(phone).or(address);
+
+        return new ArrayList<>(persons.stream().filter(combine).toList());
     }
 }
