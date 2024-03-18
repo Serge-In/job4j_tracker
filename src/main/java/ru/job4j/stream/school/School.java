371f2beaf8dee5350d21f4c2a20d6f8467469385
Serge-In <aa11@mail.ru>
2024-03-18 16:12:17 +0300

    5. Фильтрация учеников [#110058]

---
+++ src/main/java/ru/job4j/stream/school/School.java
@@ -0,0 +1,14 @@
+package ru.job4j.stream.school;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * 2024-03-18
+ * 5. Фильтрация учеников [#110058]
+ */
+public class School {
+    public List<Student> collect(List<Student> students, Predicate<Student> predicate) {
+        return students.stream().filter(predicate).toList();
+    }
+}
