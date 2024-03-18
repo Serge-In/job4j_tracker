601dceb4c71ec4255d1b8d7a9deebd2eb9365772
Serge-In <aa11@mail.ru>
2024-03-18 20:33:41 +0300

    6. Список адресов [#110062]

---
+++ src/main/java/ru/job4j/stream/client/Profiles.java
@@ -0,0 +1,19 @@
+package ru.job4j.stream.client;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 2024-03-18
+ * 6. Список адресов [#110062]
+ * В туристической компании появилась задача составить список адресов клиентов.
+ * В анкете у клиента, есть адрес проживания.
+ * Создать класс Profiles, в котором определить метод, который позволит преобразовать список профилей в список адресов.
+ * Для реализации Вам понадобятся методы: stream(), map() и collect().
+ */
+public class Profiles {
+
+    public static List<Address> collect(List<Profile> profiles) {
+        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/ru/job4j/stream/client/ProfilesTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.stream.client;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * 2024-03-18
+ * 6. Список адресов [#110062]
+ * В туристической компании появилась задача составить список адресов клиентов.
+ * В анкете у клиента, есть адрес проживания.
+ * Создать класс Profiles, в котором определить метод, который позволит преобразовать список профилей в список адресов.
+ * Для реализации Вам понадобятся методы: stream(), map() и collect().
+ */
+class ProfilesTest {
+
+    @Test
+    void whenTestMethodCollect() {
+        Address first = new Address("City1", "Street1", 1, 1);
+        Address second = new Address("City2", "Street2", 2, 2);
+        Address third = new Address("City3", "Street3", 3, 3);
+        List<Profile> profiles = Arrays.asList(
+                new Profile(first),
+                new Profile(second),
+                new Profile(third)
+        );
+        List<Address> result = Profiles.collect(profiles);
+        List<Address> expected = Arrays.asList(first, second, third);
+        assertThat(result).containsAll(expected);
+    }
+}
