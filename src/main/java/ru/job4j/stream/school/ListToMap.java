de697cc1b19d68f755c184ffc30e417b5864d862
Serge-In <aa11@mail.ru>
2024-03-21 00:58:12 +0300

    8. Преобразование List в Map. [#110226]

---
+++ src/main/java/ru/job4j/stream/school/ListToMap.java
@@ -0,0 +1,25 @@
+package ru.job4j.stream.school;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * 2024-03-18
+  * 8. Преобразование List в Map. [#110226]
+ * реализовать метод, который на вход принимает список студентов List<Student>
+ * и вернет результат его преобразования в Map<String, Student>, где:
+ * ключ - это фамилия студента;
+ * значение - объект Student (ученик).
+ * Предусмотреть в реализации исключение дубликатов,
+ * поскольку ключ - это фамилия, то дубликатами будут те пары, у которых совпадают фамилии студентов.
+ */
+public class ListToMap {
+    public static Map<String, Student> convert(List<Student> list) {
+        return list.stream()
+                .collect(Collectors
+                        .toMap(Student::getSurname, // Key mapper
+                            student -> student, // Value mapper
+                            (existingValue, newValue) -> existingValue // Merge function
+            ));
+    }
+}
